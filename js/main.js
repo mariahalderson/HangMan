d37bcd94a6343532f3f36351ffdff6524b7f76f1
Alderson <m_alderson59387@tss-hr420-dm39.local>
2018-02-28 16:43:32 -0500

    added game functionality

--- js/main.js
+++ js/main.js
@@ -5,28 +5,80 @@ var words =["party", "javascript", "coffee", "sleepy", "food"];
 
 let currentWord = words[Math.floor(Math.random()*words.length)],
     wordHint = document.querySelector('.current-word'),
-    guessBox = document.querySelector('.guess');
+    guessBox = document.querySelector('.guess'),
+    wrongGuesses = 0,
+    resetScreen = document.querySelector('.lose'),
+    resetButton = resetScreen.querySelector('button'),
+    wrongLetters = document.querySelector('.wrong-letters'),
+    wrongLetterArray = [];
 
-
+//do these things when start or restarting game
+function init() {
+  //generate a random number and grab the word that corresponds to it in the word array
 wordHint.textContent = currentWord.split("").map(letter => letter = "__").join(" ");
+
+}
+
+function showResetScreen() {
+	resetScreen.classList.add('show-piece');
+}
+
+function resetGame(){
+	wrongGuesses = 0;
+	let gamePieces = Array.from(document.querySelectorAll('.show-piece'));
+	gamePieces.forEach(piece => piece.classList.remove('show-piece'));
+	wrongLetterArray = [];
+	wrongLetters.textContent = wrongLetterArray;
+
+	init();
+}
 
 
 //functions (logic) goes in the middle
 function makeGuess() {
   console.log(this.value);
+
+  if (this.value == "" || this.value.length < 1) {
+  	return;
+  }
+
+  //set up path through app.
+  //inside round brackets, check for condition
+  if (currentWord.indexOf(this.value) < 0 ){ //if letter is at no position (not in word) , will appear as -1.
+  	//person chose wrong letter, track wrong answer.
+  	
+  	if(wrongGuesses >=6) {
+  		console.log('you lose!');
+  		//show losing screen
+  		showResetScreen();
+  	}else{
+  		//push wrong letter into array
+  	wrongLetterArray.push(this.value);
+  	wrongLetters.textContent = wrongLetterArray.join(" ");
+  	document.querySelector(`.wrongGuess${wrongGuesses}`).classList.add('show-piece');
+
+  	//incriment wrong guess count, show a piece of hangman.
+  	wrongGuesses++;//last thing in funct
+  }
+
+}else{
+	let letterGuess = currentWord.split("");
+	console.log(letterGuess.includes(this.value));
+	if(this.value == true){
+		wrongLetter.textContent = this.value;
+		//probably change this!!!
+	}
 }
+}
+
+
 
 
 //event handling goes at the bottom
 guessBox.addEventListener('keyup', makeGuess);
-
-//do these things when start or restarting game
-function init() {
-  //generate a random number and grab the word that corresponds to it in the word array
-  var random = Math.random();
+resetButton.addEventListener('click', resetGame);
 
 
-}
 
   init();
 })();
